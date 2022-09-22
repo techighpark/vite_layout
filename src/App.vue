3f461c4714e06f7f6a9a2b8dba71a67cb73ddaca
techighpark <techigh@amuz.co.kr>
2022-09-22 13:40:34 +0900

    vue-meta install

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,46 @@
 <script setup>
-import Children1 from '@/views/Children1.vue';
-import { h } from 'vue';
+import { computed, ref, watch } from 'vue';
 
-const p = h( 'div', {
-  id: 'foo',
-  innerHTML: 'hello',
-  '.name': 'some-name',
-  style: { color: 'red' },
-  onClick: () => {
-    console.log( 'click' );
-  },
+const re = new RegExp( 'ab+c' );
+const regexTest = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/;
+regexTest.test( '한글' );
+
+const count = ref( 1 );
+const plusCount = computed( {
+	get: () => count.value + 10,
+	set: ( val ) => {
+		count.value = val - 100;
+	},
 } );
+console.group( 'before' );
+// plusCount.value = plusCount.get
+console.log( 'before count : ', count.value );
+console.log( 'before computed: ', plusCount.value );
+console.groupEnd();
+// plusCount.value = * : plusCount.set
+plusCount.value = 500;
+console.group( 'after' );
+console.log( 'after count : ', count.value );
+console.log( 'after computed: ', plusCount.value );
+console.groupEnd();
 
-function render() {
-  return h( 'div', [ p ] );
-}
-
-render();
+watch( count, ( newVal, oldVal ) => {
+	console.group( 'watch' );
+	console.log( 'new', newVal );
+	console.log( 'old', oldVal );
+	console.log( count.value );
+	console.groupEnd( 'watch end' );
+} );
 </script>
 
 <template>
-  <layout>
-    <div class="border border-red-400">
-      <router-view/>
-      <router-view name="rightBar"></router-view>
-    </div>
-    <children1/>
-  </layout>
+	<layout>
+		<div class="border border-red-400">
+			<button @click="count++">button</button>
+			<!--			<router-view/>-->
+			<!--			<router-view name="rightBar"></router-view>-->
+		</div>
+		{{ re }}
+	</layout>
 </template>
 
